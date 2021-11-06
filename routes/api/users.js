c629da2dd3eb2f4419a0ad55dac35dd263d47504
Hector Siman <hector@verticis.com.br>
2021-11-06 00:20:08 -0300

    new(): User Registration system

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const {check, validationResult } = require('express-validator')
+
+const User = require('../../models/User')
 
 // @route   POST   api/users
 // @desc    Register user
@@ -16,15 +20,52 @@ router.post('/',
         'Please enter your password with more than 6 characters'
         ).isLength({min: 6})
     ], 
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req)
         if(!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
             })
         }
-    console.log(req.body)
-    res.send('User Route')
+
+        const { name, email, password } = req.body;
+
+        try {
+            // See if user exists
+            let user = await User.findOne({ email });
+            if(user) {
+                return res.status(400).json({
+                    errors: [{
+                        msg: 'User already exists'
+                    }]
+                })
+            }
+            // Get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User ({
+                name,
+                email,
+                avatar,
+                password
+            })
+            // encryt password
+            const salt = await bcrypt.genSalt(10)
+            user.password = await bcrypt.hash(password, salt);
+
+
+            await user.save()
+    
+            // Return jsonwebtoken
+            res.send('User registerd')
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send('Server error')
+        } 
 })
 
 module.exports = router
