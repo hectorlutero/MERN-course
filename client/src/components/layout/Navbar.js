cbf9747a0a0f19d72125cbbb4ea822b21fe1a919
Hector Siman <hector@verticis.com.br>
2022-02-08 22:04:24 -0300

    update(): preventing user from get back to landing page once he has logged in

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,12 +7,16 @@ import { logout } from "../../actions/auth";
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-      <li>
+      <Link to="/dashboard">
+        <i className="fas fa-user"></i>{" "}
+        <span className="hide-sm"> Dashboard</span>
+      </Link>
+      <Link to="#">
         <a onClick={logout} href="#!">
           <i className="fas fa-sign-out-alt"></i>{" "}
           <span className="hide-sm"> Logout</span>
         </a>
-      </li>
+      </Link>
     </ul>
   );
   const guestLinks = (
